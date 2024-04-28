084fe07d49efe9be2ec0c0661eb3e1f421df06bc
Anastasiya Voronina <Asyafilatova@94gmail.com>
2024-04-29 02:25:18 +0300

    Задание 44

---
+++ main.py
@@ -0,0 +1,21 @@
+import random
+import pandas as pd
+
+lst = ['robot'] * 10
+lst += ['human'] * 10
+random.shuffle(lst)
+isHuman = []
+isRobot = []
+for item in lst:
+    if item == 'robot':
+      isRobot += [True]
+    else:
+      isRobot += [False]
+
+    if item == 'human':
+      isHuman += [True]
+    else:
+      isHuman += [False]
+data = pd.DataFrame({'whoAmI':lst})
+dataOneHot = pd.DataFrame({'human':isHuman, 'robot':isRobot})
+dataOneHot.head()
